547e681564d4bbe10d4a6961396abb7d9d55101c
runner15 <therunner151@gmail.com>
2016-04-01 15:44:43 -0400

    Started changing code for new server and motor code

--- RobotCode159.ino
+++ RobotCode159.ino
@@ -1,7 +1,4 @@
 #include <MotorDriver.h>
-#include <seeed_pwm.h>
-#include <VarSpeedServo.h> 
-
 #include <QTRSensors.h>
 
 #define NUM_SENSORS             8  // number of sensors used
@@ -13,19 +10,7 @@ NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
 unsigned int sensorValues[NUM_SENSORS];
 unsigned int line_position=0; // value from 0-7000 to indicate position of line between sensor 0 - 7
 
-// ArduMoto motor driver vars
-// pwm_a/b sets speed.  Value range is 0-255.  For example, if you set the speed at 100 then 100/255 = 39% duty cycle = slow
-// dir_a/b sets direction.  LOW is Forward, HIGH is Reverse 
-int pwm_a = 10;  //PWM control for Ardumoto outputs A1 and A2 is on digital pin 10  (Left motor)
-int pwm_b = 11;  //PWM control for Ardumoto outputs B3 and B4 is on digital pin 11  (Right motor)
-int dir_a = 12;  //direction control for Ardumoto outputs A1 and A2 is on digital pin 12  (Left motor)
-int dir_b = 13;  //direction control for Ardumoto outputs B3 and B4 is on digital pin 13  (Right motor)
-
-#define servoRightPin A1
-#define servoLeftPin A0
-
-// motor tuning vars 
-int calSpeed = 165;   // tune value motors will run while auto calibration sweeping turn over line (0-255)
+MotorDriver motor;
 
 // Proportional Control loop vars
 float error=0;
@@ -35,24 +20,22 @@ int m1Speed=0; // (Left motor)
 int m2Speed=0; // (Right motor)
 
 // Servo
-VarSpeedServo servoRight;    // create servo object to control a servo 
-VarSpeedServo servoLeft;
+#define SERVO_PIN A0
+#define SERVO_PIN1 A1
 unsigned int servoTotal = 1;
+// This is the time since the last rising edge in units of 0.5us.
+uint16_t volatile servoTime = 0;
+// This is the pulse width we want in units of 0.5us.
+uint16_t volatile servoHighTime = 3000;
+// This is true if the servo pin is currently high.
+boolean volatile servoHigh = false;
 
 void setup() { // put your setup code here, to run once:
-  //Set control pins to be outputs
-  pinMode(pwm_a, OUTPUT);  
-  pinMode(pwm_b, OUTPUT);
-  pinMode(dir_a, OUTPUT);  
-  pinMode(dir_b, OUTPUT);  
+  motor.begin();
 
-  servoRight.attach(servoRightPin);
-  servoLeft.attach(servoLeftPin); 
+  servoInit();
 
-  //set both motors to stop
-  analogWrite(pwm_a, 0);  
-  analogWrite(pwm_b, 0);
-  // then start calibration phase and move the sensors over both
+  // start calibration phase and move the sensors over both
   // reflectance extremes they will encounter in your application:
   for (int i = 0; i < 250; i++)  // make the calibration take about 5 seconds
   {
@@ -151,4 +134,82 @@ void follow_line(int line_position) //follow the line
     servoLeft.write(45, 30, true); //push out
   }
 }*/
+
+// This ISR runs after Timer 2 reaches OCR2A and resets.
+// In this ISR, we set OCR2A in order to schedule when the next
+// interrupt will happen.
+// Generally we will set OCR2A to 255 so that we have an
+// interrupt every 128 us, but the first two interrupt intervals
+// after the rising edge will be smaller so we can achieve
+// the desired pulse width.
+ISR(TIMER2_COMPA_vect)
+{
+  // The time that passed since the last interrupt is OCR2A + 1
+  // because the timer value will equal OCR2A before going to 0.
+  servoTime += OCR2A + 1;
+   
+  static uint16_t highTimeCopy = 3000;
+  static uint8_t interruptCount = 0;
+   
+  if(servoHigh)
+  {
+    if(++interruptCount == 2)
+    {
+      OCR2A = 255;
+    }
+ 
+    // The servo pin is currently high.
+    // Check to see if is time for a falling edge.
+    // Note: We could == instead of >=.
+    if(servoTime >= highTimeCopy)
+    {
+      // The pin has been high enough, so do a falling edge.
+      digitalWrite(SERVO_PIN, LOW);
+      servoHigh = false;
+      interruptCount = 0;
+    }
+  } 
+  else
+  {
+    // The servo pin is currently low.
+     
+    if(servoTime >= 40000)
+    {
+      // We've hit the end of the period (20 ms),
+      // so do a rising edge.
+      highTimeCopy = servoHighTime;
+      digitalWrite(SERVO_PIN, HIGH);
+      servoHigh = true;
+      servoTime = 0;
+      interruptCount = 0;
+      OCR2A = ((highTimeCopy % 256) + 256)/2 - 1;
+    }
+  }
+}
+ 
+void servoInit()
+{
+  digitalWrite(SERVO_PIN, LOW);
+  pinMode(SERVO_PIN, OUTPUT);
+   
+  // Turn on CTC mode.  Timer 2 will count up to OCR2A, then
+  // reset to 0 and cause an interrupt.
+  TCCR2A = (1 << WGM21);
+  // Set a 1:8 prescaler.  This gives us 0.5us resolution.
+  TCCR2B = (1 << CS21);
+   
+  // Put the timer in a good default state.
+  TCNT2 = 0;
+  OCR2A = 255;
+   
+  TIMSK2 |= (1 << OCIE2A);  // Enable timer compare interrupt.
+  sei();   // Enable interrupts.
+}
+ 
+void servoSetPosition(uint16_t highTimeMicroseconds)
+{
+  TIMSK2 &= ~(1 << OCIE2A); // disable timer compare interrupt
+  servoHighTime = highTimeMicroseconds * 2;
+  TIMSK2 |= (1 << OCIE2A); // enable timer compare interrupt
+}
 
